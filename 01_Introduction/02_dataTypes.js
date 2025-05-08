28d93288cd24771bfdb228e3c96e6e59b28db28d
aditya-tripathee <adityatripatheee@gmail.com>
2025-05-08 07:58:35 +0530

    commit

---
+++ 01_Introduction/02_dataTypes.js
@@ -0,0 +1,19 @@
+// Data types :--
+// -> Primitive datatypes :-- fundamental , basic data types 
+
+// 1. Number :--It represents a numeric values 
+num = 20;
+console.log(num);
+console.log(typeof num);
+
+n = -5;
+console.log(n);
+console.log(typeof n);
+
+
+
+// 2. Strings :-- It repesents a characters which is enclosed in single quotes or double quotes.
+myName = "Aditya";
+console.log(myName);
+console.log(typeof myName);
+
