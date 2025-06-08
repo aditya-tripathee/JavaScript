f0bbdca02ae71bd0b925ea387a9639c50983da26
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-08 14:28:17 +0530

    commit

---
+++ 04_operators.js
@@ -0,0 +1,27 @@
+// Operators in JS 
+
+// Comments in JS 
+// Part of code which is not executed.
+// This is a single line comments 
+/*  
+   This is a multi line comments 
+*/
+
+// Operators :-- used to perform operations on data.
+
+// Arithmetic operators :--
+let a = 4, b = 2;
+console.log(a+b); // addition
+console.log(a-b); // sub
+console.log(a*b); // mult
+console.log(a/b); // division
+
+// Modulus :-- 
+console.log(a%b);  // remainder
+
+// Exponentation :--
+console.log(a**b);
+
+// Unary operators :--
+
+
