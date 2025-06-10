eeaddaeb51eb5cc07611a9d6ec91a014a2d3d645
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-10 14:07:40 +0530

    commit

--- 09_Functions.js
+++ 09_Functions.js
@@ -1,1 +1,17 @@
 // Functions in JS 
+// Block of code that perfoems a specific task, can be invoked whenever needed.
+
+// Function Definition  
+ function myName(){
+    console.log("My name is Aditya");
+    
+ }
+
+//  function call 
+  myName();
+  myName();
+  myName();
+  myName();
+
+
+
