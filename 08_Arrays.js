5a1d0c3888ddf66bd378653166fc13ee61d90501
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-10 12:43:28 +0530

    commit

---
+++ 08_Arrays.js
@@ -0,0 +1,10 @@
+// Arrays in JS 
+// Collections of items 
+
+
+// create a array
+let heroes = ["spiderman","superman","hulk","shaktiman"];
+console.log(heroes);
+console.log(typeof heroes);
+
+console.log(heroes.length);
