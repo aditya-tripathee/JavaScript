9969084be6681a52aa8475746b7be5563b1513e7
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-10 12:48:17 +0530

    commit

--- 08_Arrays.js
+++ 08_Arrays.js
@@ -7,4 +7,14 @@ let heroes = ["spiderman","superman","hulk","shaktiman"];
 console.log(heroes);
 console.log(typeof heroes);
 
+let marks = [98,56,87,26,87];
+console.log(marks);
+
+
+// length :-- 
 console.log(heroes.length);
+
+// Array indices :-- 
+console.log(heroes[2]);  
+
+// 
