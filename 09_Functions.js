58b2500248eee02e0092171ea56c52ae96745070
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-11 11:57:23 +0530

    commit

--- 09_Functions.js
+++ 09_Functions.js
@@ -7,11 +7,51 @@
     
  }
 
+// -> function -> it is keyword 
+//  -> myName -> function name
+//  console.log() -> it is used to print or runs the block of code
+
 //  function call 
   myName();
   myName();
   myName();
   myName();
 
+//   Function parameters and function arguments
 
+  function greet(name,age){  //-> name is parameters
+    console.log(`My name is ${name} and age is ${age}`);
+    
+  }
+  greet("adii",20);  //-> arguments
+
+
+
+//   sum two numbers 
+function sum(x,y){
+    console.log(x+y);
+    
+}
+sum(10,5);
+
+
+
+// return keyword 
+
+function sub(x,y){
+    return x-y;
+}
+
+console.log(sub(10,5));
+
+
+// Arrow functions :-- compact way to writing a function
+
+const add = (x,y)=>{
+    // console.log(x+y);
+    return x + y;
+    
+}
+
+console.log(add(10,50));
 
