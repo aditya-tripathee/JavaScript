eadd3397790714aad948c28c9a8b152579f68f61
aditya-tripathee <adityatripatheee@gmail.com>
2025-06-14 14:45:45 +0530

    commit

---
+++ DOM/script.js
@@ -0,0 +1,30 @@
+// alert("user hacked!");
+
+
+/* Window objects :-- It represents a open window in a browser. It is a browser object(not javascript objects) and it is automatically created by 
+  browser. It is a global objects with a lots of properties and methods.
+
+
+// */
+// console.log(window);
+// window.console.log("hello");
+
+// window.alert("error");
+
+
+
+// DOM :--  Document Object Model
+// When a page is loaded, the browser creates a Document Object Model (DOM) of the page.
+
+
+// console.log(window.document);
+// console.dir(window.document);
+
+
+console.log(window.document.body);
+console.dir(window.document.body.childNodes[1]);
+// console.dir(document.head);
+// console.log(document.head);
+
+
+
